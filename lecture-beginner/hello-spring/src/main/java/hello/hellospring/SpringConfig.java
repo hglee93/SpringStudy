c4bf0aa34186ac5c6aa56aead943fd631c8c984e
hglee93 <jeuslhg@gmail.com>
2021-05-09 20:03:30 +0900

    Add Java code Spring Config
    - Using Configuration, Register bean class to Spring Container

---
+++ lecture-beginner/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hellospring;
+
+import hello.hellospring.Repository.MemberRepository;
+import hello.hellospring.Repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+}
