386ee09d0f38a6bceb1b10d37a6614c220761a6c
hglee93 <jeuslhg@gmail.com>
2021-05-10 23:58:07 +0900

    Add jdbc
    - Add jdbc Template

--- lecture-beginner/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ lecture-beginner/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,10 +1,14 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * Configuration 파일
@@ -16,6 +20,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -23,7 +34,8 @@ public class SpringConfig {
 
     @Bean
     MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
