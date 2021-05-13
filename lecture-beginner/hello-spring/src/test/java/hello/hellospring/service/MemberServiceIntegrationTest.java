c775f2b7bd9b87a3ce3026eb5e1503a8d9354452
hglee93 <jeuslhg@gmail.com>
2021-05-13 23:07:05 +0900

    Add SpringIntegrationTest
    - @SpringBootTest : Execute test case with Spring Container
    - @Transactional : Rollback SQL query in test cases after testcase terminates

---
+++ lecture-beginner/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,69 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void join() {
+        // 테스트 케이스는 항상 given, when, then을 생각하자
+        // given : 어떤 데이터가 주어지고(given)
+        // when : 어떤 Action을 실행했을 때(when)
+        // then : 그때 결과가 맞는가?(then)
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member result = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(result.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+
+        // try-catch로 Exception을 처리해도 되지만~,
+        // assertThrows로도 예상한 Exception이 발생하는지 확인할 수 있다.
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+
+
+}
