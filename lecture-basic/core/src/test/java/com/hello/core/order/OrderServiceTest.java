5b325013f945a432ccb4bf4d242f7837958272bc
hglee93 <jeuslhg@gmail.com>
2021-05-31 07:38:46 +0900

    새로운 할인정책 추가
    - RateDiscountPolicy 추가
    - 할인정책의 다형성
    - 테스트 코드 추가

---
+++ lecture-basic/core/src/test/java/com/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package com.hello.core.order;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void order() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
