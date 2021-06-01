ae80c6d490ad63fc8b56fdf19dd2b6619d4c6092
hglee93 <jeuslhg@gmail.com>
2021-06-01 22:33:22 +0900

    Change DiscountPolicy
    - AppConfig를 통해 얻은 이점
    -> 구현체 변경이 발생했을 때, 클라이언트 코드의 변경이 일어나지 않았다.
    -> 이로 인해 프로그램의 코드가 사용영역(실행영역), 설정영역으로 나뉘게 되었다.

--- lecture-basic/core/src/main/java/com/hello/core/AppConfig.java
+++ lecture-basic/core/src/main/java/com/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package com.hello.core;
 
 import com.hello.core.discount.DiscountPolicy;
 import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.discount.RateDiscountPolicy;
 import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
@@ -20,7 +21,7 @@ public class AppConfig {
     }
 
     private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
     private MemberRepository memberRepository() {
--- lecture-basic/core/src/main/java/com/hello/core/OrderApp.java
+++ lecture-basic/core/src/main/java/com/hello/core/OrderApp.java
@@ -1,6 +1,9 @@
 package com.hello.core;
 
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
 import com.hello.core.member.MemberService;
+import com.hello.core.order.Order;
 import com.hello.core.order.OrderService;
 
 public class OrderApp {
@@ -8,5 +11,14 @@ public class OrderApp {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
         OrderService orderService = appConfig.orderService();
+        MemberService memberService = appConfig.memberService();
+
+        Member member = new Member(1L, "member", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(member.getId(), "itemA", 20000);
+
+        System.out.println("order : " + order.toString());
+
     }
 }
