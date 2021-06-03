499e984c21a0c4f496945cd22695b09715aa20ec
hglee93 <jeuslhg@gmail.com>
2021-06-03 22:31:34 +0900

    Add findAllBeans, findApplicationBeans Test
    - 스프링 컨테이너에서 모든 빈 조회해보기
    - 스프링 컨테이너에서 어플리케이션 빈 조회해보기

--- lecture-basic/core/src/main/java/com/hello/core/MemberApp.java
+++ lecture-basic/core/src/main/java/com/hello/core/MemberApp.java
@@ -3,11 +3,22 @@ package com.hello.core;
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
 import com.hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+
+        // Spring 컨테이너 생성
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        /**
+         * parameter
+         * - bean name
+         * - type
+         */
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- lecture-basic/core/src/main/java/com/hello/core/OrderApp.java
+++ lecture-basic/core/src/main/java/com/hello/core/OrderApp.java
@@ -5,13 +5,24 @@ import com.hello.core.member.Member;
 import com.hello.core.member.MemberService;
 import com.hello.core.order.Order;
 import com.hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        OrderService orderService = appConfig.orderService();
-        MemberService memberService = appConfig.memberService();
+
+        // Generate Spring Container
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // get memberService bean and orderService bean
+        // * Pass AppConfig and Generate applicationContext
+        // * So Spring Container register bean, that written on AppConfig Class, to Spring Bean Store
+        // * In Configuration Class, the method name becomes bean name. Return type becomes bean type.
+        // * 이렇게 하기 위해서는, AppConfig 클래스에 @Configuration 어노테이션 생성, 빈을 스프링 컨테이너에 등록하기 위해서는 @Bean을 명시해주어야 한다.
+        // * Bean name must be unique. otherwise side effect happens.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Member member = new Member(1L, "member", Grade.VIP);
         memberService.join(member);
