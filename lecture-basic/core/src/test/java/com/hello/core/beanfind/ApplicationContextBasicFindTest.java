05b9bd60e3eb8f57e26ff1500821a948e52f206b
hglee93 <jeuslhg@gmail.com>
2021-06-04 23:02:41 +0900

    Add SearchForSpringBean Test
    - 해당 코드가 중요한 이유? @Autowired 등 스프링이 DI할 때 해당 방식으로 조회해서 DI한다..
    - Name, Type, Implementation으로 조회해보기

---
+++ lecture-basic/core/src/test/java/com/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,47 @@
+package com.hello.core.beanfind;
+
+import com.hello.core.AppConfig;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("이름으로 빈 조회하기")
+    void findByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("타입으로 빈 조회하기")
+    void findByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 빈 조회하기")
+    void findByName2() {
+        MemberServiceImpl memberService = ac.getBean(MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class, () -> {
+            MemberService xxxxxx = ac.getBean("xxxxxx", MemberService.class);
+        });
+    }
+}
