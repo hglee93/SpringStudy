2d64aeb840a1839600d523911f9991f4d6829153
hglee93 <jeuslhg@gmail.com>
2021-06-06 22:26:27 +0900

    스프링컨테이너에서 스프링빈을 실제로 싱글턴객체로 생성하는지 확인

--- lecture-basic/core/src/test/java/com/hello/core/singleton/SingletonTest.java
+++ lecture-basic/core/src/test/java/com/hello/core/singleton/SingletonTest.java
@@ -2,7 +2,6 @@ package com.hello.core.singleton;
 
 import com.hello.core.AppConfig;
 import com.hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
