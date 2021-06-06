02e131ae323d9c6cd1d6dce0aecf884e6a86a28b
hglee93 <jeuslhg@gmail.com>
2021-06-06 17:20:12 +0900

    싱글톤 패턴 예제
    - 싱글톤은 클래스의 인스턴스가 1개만 존재하는 패턴을 말한다.
    - 웹 어플리케이션에서 요청이 들어올 때마다 객체를 생성하는 것이 아니라 미리 인스턴스를 생성함으로써 객체 생성 비용을 줄일 수 있다.
    - 위와 같이 객체를 공유하는 장점이 있지만, 테스트 코드 작성이 어렵고 구현에 의존하며 클라이언트 코드가 변경된다는 단점이 존재한다.

---
+++ lecture-basic/core/src/test/java/com/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,38 @@
+package com.hello.core.singleton;
+
+import com.hello.core.AppConfig;
+import com.hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 DI컨테이너는 서로 다른 인스턴스를 생성한다.")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 객체 테스트")
+    void singletonService() {
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        System.out.println("instance1 = " + instance1);
+        System.out.println("instance2 = " + instance2);
+
+        assertThat(instance1).isSameAs(instance2);
+    }
+}
