17eb3d1c468e8fb3b6ad63696e3b24660e95e9c6
hglee93 <jeuslhg@gmail.com>
2021-06-14 06:47:24 +0900

    Add 인터페이스를 이용한 생명주기 콜백 사용TEST
    - 스프링 초창기에 나온 방법
    - 해당 콜백은 외부 코드를 사용할 때 사용하기 힘들다는 단점이 있음

--- lecture-basic/core/src/test/java/com/hello/core/lifecycle/NetworkClient.java
+++ lecture-basic/core/src/test/java/com/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package com.hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,4 +27,16 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    // 의존관계 주입이 끝나면 실행
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
 }
